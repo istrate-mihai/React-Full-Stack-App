9bbcaa1a72d04ef85687908074514bbf42ca3080
Istrate-Mihai <i.mihai9960@gmail.com>
2025-05-28 11:59:02 +0300

    Added functionality for article upvote and comments

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,40 @@
 import express from 'express';
 
+const articleInfo = [
+  { name: 'learn-node', upvotes: 0, comments: [] },
+  { name: 'learn-react', upvotes: 0, comments: [] },
+  { name: 'mongodb', upvotes: 0, comments: [] },
+];
+
 const app = express();
 
-app.get('/get', function (req, res) {
-  res.send('Hello');
+app.use(express.json());
+
+app.post('/api/articles/:name/upvote', function (req, res) {
+  const article = articleInfo.find(
+    (article) => article.name === req.params.name
+  );
+  article.upvotes += 1;
+  res.send(
+    'Hooray, success! The article ' +
+      req.params.name +
+      ' now has ' +
+      article.upvotes +
+      ' upvotes!'
+  );
+});
+
+app.post('/api/articles/:name/comments', function (req, res) {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articleInfo.find((article) => article.name === name);
+  article.comments.push({
+    postedBy,
+    text,
+  });
+
+  res.json(article);
 });
 
 app.listen(8000, function () {
